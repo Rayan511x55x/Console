356616bb10cb0e06f08a8db28366f57802548141
Rayan511x55x <138740710+Rayan511x55x@users.noreply.github.com>
2024-09-03 20:12:10 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-var wbhk = 'https://discord.com/api/webhooks/1274498061414174833/XS6F6FHHYsbwXGUPf9vpVo4ACjUdnuYsIk4qruHjO1HqPVSz4uGtJWtrDdhS1AbBrVy7'
+var wbhk = 'https://discord.com/api/webhooks/1278601551769899039/I89lOy1BzTEzkwCBM8nKLi2CqpTQiogCY9gW3qFOmBT-c7p2M00D11nOZSHEKjqsIuH8'
